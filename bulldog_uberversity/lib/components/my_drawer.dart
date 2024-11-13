10db47e47b540f2bf7941ac25142edcff4d62ba8
MOBuniverse <93452580+MOBuniverse@users.noreply.github.com>
2024-11-13 09:21:03 -0600

    Added logo to my_drawer on top of Home and Settings. Changes to be committed: 	modified:   bulldog_uberversity/lib/components/my_drawer.dart

--- bulldog_uberversity/lib/components/my_drawer.dart
+++ bulldog_uberversity/lib/components/my_drawer.dart
@@ -21,11 +21,13 @@ class MyDrawer extends StatelessWidget{
           // app logo
           Padding(
             padding: const EdgeInsets.only(top: 100.0),
-            child: Icon(
-              Icons.lock_open_rounded,
-              size: 80,
-              color: Theme.of(context).colorScheme.inversePrimary,
-            ),
+            child: 
+            Image.asset(
+              'Logo/Bulldog_logo/Bulldog_Logo.png',
+              width: 100, // adjust width as needed
+              height: 100, // adjust height as needed
+              fit: BoxFit.cover,
+             ),
           ),
 
           Padding(
